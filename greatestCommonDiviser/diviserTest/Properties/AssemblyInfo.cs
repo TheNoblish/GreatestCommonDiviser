d66b0c98e6e287debabb372fba4712ef96f13390
TheNoblish <DrNoble2.0@gmail.com>
2020-03-28 15:43:59 +0100

    yikes

---
+++ greatestCommonDiviser/diviserTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("diviserTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("diviserTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d7601cad-16ae-46c0-83c0-c0fbd229fed2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ greatestCommonDiviser/greatestCommonDiviser/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greatestCommonDiviser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(FindDiviser(45, 10));
+            Console.ReadLine();
+           
+
+            
+
+
+        }
+        public static int FindDiviser(int a, int b)
+        {
+            int diviser;
+            int left = a;
+            int right = b;
+            do
+            {
+                diviser = remainder(left, right);
+                left = right;
+                right = diviser;
+            } while (diviser != 0);
+
+            return left;
+        }
+        public static int remainder(int a, int b)
+        {
+            return a % b;
+        }
+    }
+}
