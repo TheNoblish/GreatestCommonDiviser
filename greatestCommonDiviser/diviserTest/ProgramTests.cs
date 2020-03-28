9de1e41fba7328a8d65e781dccc024e698c06dff
TheNoblish <DrNoble2.0@gmail.com>
2020-03-28 17:17:53 +0100

    working program

--- greatestCommonDiviser/diviserTest/ProgramTests.cs
+++ greatestCommonDiviser/diviserTest/ProgramTests.cs
@@ -11,10 +11,42 @@ namespace greatestCommonDiviser.Tests
     [TestClass()]
     public class ProgramTests
     {
+        // full testing documentation at https://docs.google.com/document/d/1_BSmvXBNifrJA0DAPVz0RLJbs-tf0k5k22BI4v5wqew/edit?usp=sharing
+
         [TestMethod()]
-        public void MainTest()
+        public void InputToDivisorTestA()
         {
-            Assert.Fail();
+            Assert.AreEqual(Program.InputToDivisor(""), "Sorry, it doesn't look like you entered two integers.");
+        }
+        [TestMethod()]
+        public void InputToDivisorTestB()
+        {
+            Assert.AreEqual(Program.InputToDivisor("3"), "Sorry, it doesn't look like you entered two integers.");
+        }
+        [TestMethod()]
+        public void InputToDivisorTestC()
+        {
+            Assert.AreEqual(Program.InputToDivisor("2 4 6"), "Sorry, it doesn't look like you entered two integers.");
+        }
+        [TestMethod()]
+        public void InputToDivisorTestD()
+        {
+            Assert.AreEqual(Program.InputToDivisor("ha ha"), "Sorry, it doesn't look like you entered two integers.");
+        }
+        [TestMethod()]
+        public void InputToDivisorTestE()
+        {
+            Assert.AreEqual(Program.InputToDivisor("6 6"), "The greatest common divisor of 6 and 6 is 6.");
+        }
+        [TestMethod()]
+        public void InputToDivisorTestF()
+        {
+            Assert.AreEqual(Program.InputToDivisor("10 15"), "The greatest common divisor of 10 and 15 is 5.");
+        }
+        [TestMethod()]
+        public void InputToDivisorTestG()
+        {
+            Assert.AreEqual(Program.InputToDivisor("-22 16"), "The greatest common divisor of -22 and 16 is 2.");
         }
 
     }
--- greatestCommonDiviser/greatestCommonDiviser/Program.cs
+++ greatestCommonDiviser/greatestCommonDiviser/Program.cs
@@ -10,19 +10,74 @@ namespace greatestCommonDiviser
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(FindDiviser(45, 10));
-            Console.ReadLine();
-           
+            // this program uses euclidian methodology to calculate the greatest common divisor of two integers
 
+            // this section should be pretty familiar at this point, being the standard methods of user input we've used before
+            Console.WriteLine("Welcome to the Eucliditron 5000");
+            Console.WriteLine("");
+            while (true)
+            {
+                Console.WriteLine("What would you like to do? (calculate, quit)");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "calculate":
+                        {
+                            Console.WriteLine("Please write the two integers you'd like to calculate for.");
+                            string numb = Console.ReadLine();
+                            // turning the input to the divisor is made as a seperate function for ease of testing
+                            Console.WriteLine(InputToDivisor(numb));
+                            break;
+                        }
+                    case "quit":
+                        {
+                            Environment.Exit(0);
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("That doesn't look like a valid command");
+                            break;
+                        }
+                }
+
+            }
             
 
-
         }
-        public static int FindDiviser(int a, int b)
+        public static string InputToDivisor(string numb)
         {
+            // splitting the input into seperate strings and checking if we have two
+            string[] numbs = numb.Split(' ');
+            if (numbs.Length == 2)
+            {
+                // the try catch function will attempt to do what is in try, and then move to catch if it fails to do something. 
+                // for example if one of the inputs cant be made to an int it will jump out of try and into catch.
+                try
+                {
+                    int a = Int32.Parse(numbs[0]);
+                    int b = Int32.Parse(numbs[1]);
+                    // finally this section returns the solution using the finddivisor function
+                    return ("The greatest common divisor of " + a + " and " + b + " is " + FindDivisor(a, b) + ".");
+                }
+                catch
+                {
+                   return ("Sorry, it doesn't look like you entered two integers.");
+                }
+            }
+            else
+            {
+                return ("Sorry, it doesn't look like you entered two integers.");
+            }
+        }
+        public static int FindDivisor(int a, int b)
+        {
+            
             int diviser;
-            int left = a;
-            int right = b;
+            int left = Math.Abs(a);
+            int right = Math.Abs(b);
+            // this do while loop covers the euclidian algorithm for finding the greatest common divisor
+            // an indepth explanation of the algorithm can be found here: https://www.youtube.com/watch?time_continue=93&v=JUzYl1TYMcU&feature=emb_title
             do
             {
                 diviser = remainder(left, right);
@@ -32,8 +87,10 @@ namespace greatestCommonDiviser
 
             return left;
         }
+        
         public static int remainder(int a, int b)
         {
+            // this function just calculates the remainder of two ints, theres no real reason for it to be a seperate function in this version of the program
             return a % b;
         }
     }
